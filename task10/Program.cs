b77eefb979535da1a781958b97fd95949eb851bf
denis aloviagin <d.aloviagin@gmail.com>
2023-02-16 02:03:33 +0500

    sem2

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трёхзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введённое число `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра `{secondRank}`");
