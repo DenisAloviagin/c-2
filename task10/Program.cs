8136a6662880f444e77be9e84e643962ce55e253
denis aloviagin <d.aloviagin@gmail.com>
2023-02-16 02:40:58 +0500

    seminar2

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,17 +1,22 @@
-﻿int Prompt (string message)
+﻿internal class Program
 {
-    System.Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
+    private static void Main(string[] args)
+    {
+        int Prompt(string message, string? value)
+        {
+            Console.Write(message);
+            int result = Convert.ToInt32(value);
+            return result;
+        }
 
-int number = Prompt("Введите трёхзначное число > ");
-if (number < 100 || number >= 1000)
-{
-    Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста повторите ввод");
-    return;
+        int number = Prompt("Введите трёхзначное число > ", Console.ReadLine());
+        if (number < 100 || number >= 1000)
+        {
+            Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста повторите ввод");
+            return;
+        }
+        Console.WriteLine($"Введённое число `{number}`");
+        int secondRank = number / 10 % 10;
+        Console.WriteLine($"Вторая цифра `{secondRank}`");
+    }
 }
-Console.WriteLine($"Введённое число `{number}`");
-int secondRank = number / 10 % 10;
-Console.WriteLine($"Вторая цифра `{secondRank}`");
